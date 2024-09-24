bd3972eb4480d1fa69fe9a285c9d1a380a240c9c
Dan Fiumara <dan@fedora.danLaptop>
2024-09-24 17:58:08 -0400

    add laptop perf tests with data access granularity modulated

--- proj1/run_test.sh
+++ proj1/run_test.sh
@@ -10,6 +10,19 @@ today_date=$(date +%Y-%m-%d)
 filename="${cpu_model// /_}${today_date}.txt"
 
 # Run lscpu, remove the Vulnerabilities section, and save to file
-
+#lscpu | head -n 26 > $filename
 # Inform the user where the file is saved
 echo "lscpu output saved to $filename"
+
+byte_size=1
+
+# Final test should be 20
+for i in {0..20}
+  do
+    test_file_name="./laptop_cpu_tests/${today_date}_test_${byte_size}.txt"
+    byte_size=$((byte_size*2))
+    echo "Current byte size: $byte_size"
+    echo 0 > /proc/sys/kernel/nmi_watchdog
+    perf stat ./test.o $byte_size 2> $test_file_name
+    echo 1 > /proc/sys/kernel/nmi_watchdog
+  done
